c1a61a228737d3ce8d7824eb8a65b648371f0ace
Daniel Cruz <adm.dcf@gmail.com>
2022-03-03 17:51:03 -0300

    add router and store

---
+++ src/components/Art.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { useArticlesStore } from "../store/article";
+const store = useArticlesStore();
+</script>
+<template>
+  <h1>{{ $route.params.id }}</h1>
+  <!-- <h1>{{ store.getArticlesById($route.params.id) }}</h1> -->
+  <h1>{{ store.getArticleById($route.params.id) }}</h1>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from './components/Home.vue'
-import Article from './components/Article.vue'
+import Home from "./components/Home.vue";
+import Article from "./components/Article.vue";
+import Art from "./components/Art.vue";
+
 const routes = [
-    { path: '/', component: Home },
-    { path: '/article', component: Article },
-]
+  { path: "/", component: Home },
+  { path: "/article", component: Article },
+  { path: "/art/:id", component: Art },
+];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes: routes})
+  history: createWebHistory(),
+  routes: routes,
+});
 
-    export { router as default }
+export { router as default };
