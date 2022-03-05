ce92c15bfc89b066f6b6818255a8d59d59afdbd7
Daniel <adm.dcf@gmail.com>
2022-03-05 09:09:44 -0300

    remove dead code

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./components/Home.vue";
 import Article from "./components/Article.vue";
-import Art from "./components/Art.vue";
 
 const routes = [
   { path: "/", name: "home", component: Home },
